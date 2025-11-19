d127afb64ecd1abf748bcc4d3d10da9503b72d0c
Arkadiusz Galas <arkadiusz.galas@gmail.com>
2025-11-19 20:42:27 +0100

    Adding second operation-subtracting.

--- calculator.py
+++ calculator.py
@@ -1,14 +1,21 @@
 def add(num1,num2):
    return num1 + num2
 
+def subtract(num1,num2):
+   return num1 - num2
+
 first_num = float(input("What's the first number?:"))
-print(f"First number is {first_num}")
 print("+")
 print("-")
 print("*")
 print("/")
 operation_sel = input("Pick an operation: ")
 second_num = float(input("What's the next number?: "))
+
+result = 0
 if operation_sel == "+":
    result = add(first_num,second_num)
-   print(f"Result is: {result}")
+elif operation_sel == "-":
+   result = subtract(first_num,second_num)
+
+print(f"{first_num} {operation_sel} {second_num} = {result}")
