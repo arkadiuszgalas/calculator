42f14196027bab46c81a766e18f8ba7e72672bba
Arkadiusz Galas <arkadiusz.galas@gmail.com>
2025-11-22 14:50:06 +0100

    Adding two operations - multiply and divide.

--- calculator.py
+++ calculator.py
@@ -4,6 +4,12 @@ def add(num1,num2):
 def subtract(num1,num2):
    return num1 - num2
 
+def multiply(num1,num2):
+   return num1 * num2
+
+def divide(num1,num2):
+   return num1 / num2
+
 first_num = float(input("What's the first number?:"))
 print("+")
 print("-")
@@ -17,5 +23,9 @@ if operation_sel == "+":
    result = add(first_num,second_num)
 elif operation_sel == "-":
    result = subtract(first_num,second_num)
+elif operation_sel == '*':
+   result = multiply(first_num,second_num)
+elif operation_sel == '/':
+   result = divide(first_num,second_num)
 
 print(f"{first_num} {operation_sel} {second_num} = {result}")
