a2b1e60416e704bf42e3cf3b83ecd9999ded1a17
Arkadiusz Galas <arkadiusz.galas@gmail.com>
2025-11-24 21:40:45 +0100

    Adding logic for continue calculating with result value.

--- calculator.py
+++ calculator.py
@@ -11,21 +11,30 @@ def divide(num1,num2):
    return num1 / num2
 
 first_num = float(input("What's the first number?:"))
-print("+")
-print("-")
-print("*")
-print("/")
-operation_sel = input("Pick an operation: ")
-second_num = float(input("What's the next number?: "))
+continue_calc_with_output = True
 
-result = 0
-if operation_sel == "+":
-   result = add(first_num,second_num)
-elif operation_sel == "-":
-   result = subtract(first_num,second_num)
-elif operation_sel == '*':
-   result = multiply(first_num,second_num)
-elif operation_sel == '/':
-   result = divide(first_num,second_num)
+while continue_calc_with_output:
+   print("+")
+   print("-")
+   print("*")
+   print("/")
+   operation_sel = input("Pick an operation: ")
+   second_num = float(input("What's the next number?: "))
 
-print(f"{first_num} {operation_sel} {second_num} = {result}")
+   result = 0
+   if operation_sel == "+":
+      result = add(first_num,second_num)
+   elif operation_sel == "-":
+      result = subtract(first_num,second_num)
+   elif operation_sel == '*':
+      result = multiply(first_num,second_num)
+   elif operation_sel == '/':
+      result = divide(first_num,second_num)
+
+   print(f"{first_num} {operation_sel} {second_num} = {result}")
+   continue_calc_with_output_check = input(f"Type 'y' to continue calculating with {result}, ir type 'n' to start a new calculation:")
+   if continue_calc_with_output_check.upper() == 'N':
+      continue_calc_with_output = False
+   else:
+      continue_calc_with_output = True
+      first_num = result
